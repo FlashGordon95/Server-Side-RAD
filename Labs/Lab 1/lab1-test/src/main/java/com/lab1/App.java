315a18ef0a11029a825b7b30f501e067f2147f23
FlashGordon95 <ryangordon210@gmail.com>
2017-02-02 21:23:16 +0000

    Lab 1
    Code and document for lab 1

---
+++ Labs/Lab 1/lab1-test/src/main/java/com/lab1/App.java
@@ -0,0 +1,7 @@
+package com.lab1;
+public class App {
+	public static void main(String[] args) {
+		Person person = new Person();
+		person.sayHello();
+		}
+}
---
+++ Labs/Lab 1/lab1-test/src/main/java/com/lab1/Person.java
@@ -0,0 +1,6 @@
+package com.lab1;
+public class Person {
+		public void sayHello() {
+			System.out.println("Hello from Person");
+		}
+}
